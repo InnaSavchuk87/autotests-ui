15b38398f19d782b32e230e255b0a977387f9adc
InnaSavchuk87 <Fly-7@mail.ru>
2026-04-18 14:06:47 +0300

    Плагины Pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -7,14 +7,17 @@ from playwright.sync_api import  Page, Playwright
 def chromium_page(playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
+    browser.close()
 
 
 @pytest.fixture(scope="session")
-def initialize_browser_state(playwright: Playwright) -> None:
+def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
+
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
 
@@ -38,3 +41,4 @@ def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -
     context = browser.new_context(storage_state='browser-state.json')
 
     yield context.new_page()
+    browser.close()
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import Playwright, expect, Page
+from playwright.sync_api import  expect, Page
 
 @pytest.mark.courses
 @pytest.mark.regression
