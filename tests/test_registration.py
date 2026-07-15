5a548a98c2df7bf4b9968a54dcada631571ac48d
InnaSavchuk87 <Fly-7@mail.ru>
2026-07-15 14:52:47 +0300

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -10,12 +10,12 @@ from pages.dashboard_page import DashboardPage
 def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
 
         registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        registration_page.fill_registration_form(
+        registration_page.registration_form.fill(
                 email="user.name@gmail.com",
                 username="username",
                 password="password"
         )
         registration_page.click_registration_button()
-        dashboard_page.check_visible_dashboard_title()
+        dashboard_page.dashboard_title.check_visible()
 
 
